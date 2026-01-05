015a01e4fc335ae35890eaaef937b791a851c1ca
Anhar.Ali <anhar.ali@solihull.ac.uk>
2026-01-05 11:04:45 +0000

    added new code to this file

--- howto-variables.py
+++ howto-variables.py
@@ -5,4 +5,14 @@ age = 21
 occupation = "Student"
 height = 6.1
 
-print("Hello", name, " you are ", age, " years old ", " you are currently a ", occupation, " you are ", height, " feet tall")
+print("Hello ", name, " you are ", age, " years old ", " you are currently a ", occupation, " you are ", height, " feet tall")
+
+# now lets use the same variables with different values
+
+name = "Bonny"
+age = 12
+occupation = "School Student"
+height = 4.2
+
+print("Hello ", name, " you are ", age, " years old ", " you are currently a ", occupation, " you are ", height, " feet tall")
+
