33a200ea7d8894dd40f8ef199bf9def4c4ca58f4
Anhar.Ali <anhar.ali@solihull.ac.uk>
2026-01-05 11:13:50 +0000

    a new function script is added

---
+++ howto-functions.py
@@ -0,0 +1,13 @@
+# how to work with functions in python a basic overview
+# this fucntion below will simply print out Hello in the console.
+def showHello():
+    print("Hello")
+
+showHello()
+
+# we can add variables to functions to be used as arguments see example below
+# this one will take 1 string as a argument and then add that to the final print function
+def showHello_2(name):
+    print("Hello, ", name, " Nice to meet you")
+
+showHello_2("Lebron James") # say hello
