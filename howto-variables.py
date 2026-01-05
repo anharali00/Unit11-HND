83a6ef132185327755484f6170a72955f97f90cc
Anhar.Ali <anhar.ali@solihull.ac.uk>
2026-01-05 11:01:12 +0000

    new file created

---
+++ howto-variables.py
@@ -0,0 +1,8 @@
+# How to use different variables in Python
+
+name = "Jack"
+age = 21
+occupation = "Student"
+height = 6.1
+
+print("Hello", name, " you are ", age, " years old ", " you are currently a ", occupation, " you are ", height, " feet tall")
