bc4fdc316a77522cba0903b6200ffc6b94ac7da7
Anhar.Ali <anhar.ali@solihull.ac.uk>
2026-02-23 12:09:05 +0000

    a new function script is added

--- howto-functions.py
+++ howto-functions.py
@@ -6,8 +6,16 @@ def showHello():
 showHello()
 
 # we can add variables to functions to be used as arguments see example below
-# this one will take 1 string as a argument and then add that to the final print function
+# this one will take 1 string as an argument and then add that to the final print function
 def showHello_2(name):
     print("Hello, ", name, " Nice to meet you")
 
 showHello_2("Lebron James") # say hello
+
+# function with multiple arguments
+# we can use any data type for each for example number, string even lists can be passed it
+
+def adding_two_values(numberA, numberB):
+    print("Total is", numberA + numberB)
+
+adding_two_values(2,5)
